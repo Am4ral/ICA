e78f264580cdf0d43b5e269453aa868a2e82d1ae
Am4ral <tuliomtxe@gmail.com>
2020-02-08 12:36:31 -0300

    Commint 2

--- app.js
+++ app.js
@@ -4,3 +4,8 @@
 window.addEventListener(`load`, () =>{
    const preloader = document.querySelector(`.preloader`);
    preloader.classList.add(`preloader-finish`);});
+
+
+   function dropMenu(){
+      document.getElementById("sub-menu").classList.toggle("show");
+    }
